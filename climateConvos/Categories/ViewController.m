4274a16a4e78b36c468bd3a878a1e2b105341af4
Oliver Lechuga <olechuga@uw.edu>
2018-02-28 14:50:48 -0800

    cgcngcng

--- climateConvos/Categories/ViewController.m
+++ climateConvos/Categories/ViewController.m
@@ -16,6 +16,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    
+    UIScrollView* scrollView = [[UIScrollView alloc]initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height)];
+    //scrollView.backgroundColor = [UIColor blueColor];
+    
+    [self.view addSubview:scrollView];
+    
+    [scrollView setContentSize:CGSizeMake(scrollView.bounds.size.width,scrollView.bounds.size.height*1.2)];
+    
+    [scrollView addSubview:Button1];
+    [scrollView addSubview:Button2];
+    [scrollView addSubview:Button3];
+    [scrollView addSubview:Button4];
+    [scrollView addSubview:Button5];
+    
+    
+    // old scroll view data UIView* redView = [[UIView alloc]initWithFrame:CGRectMake(0, 0, scrollView.frame.size.width, scrollView.frame.size.height)];
+                       //redView.backgroundColor = [UIColor redColor];
+                      // [self.view addSubview:redView];
+    
     // Do any additional setup after loading the view, typically from a nib.
 }
 
