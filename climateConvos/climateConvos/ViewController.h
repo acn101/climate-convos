0d18aef317de14d6d9d1e8d4a1bb26e16c9844b3
Oliver Lechuga <olechuga@uw.edu>
2018-03-02 15:41:37 -0800

    formerge

---
+++ climateConvos/climateConvos/ViewController.h
@@ -0,0 +1,25 @@
+//
+//  ViewController.h
+//  Categories
+//
+//  Created by Oliver on 2/20/18.
+//  Copyright © 2018 SASON. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController {
+
+
+IBOutlet UIButton *Button1;
+IBOutlet UIButton *Button2;
+IBOutlet UIButton *Button3; IBOutlet UIButton *Button4;
+IBOutlet UIButton *Button5;
+IBOutlet UIButton *Button6;
+
+IBOutlet UIScrollView *scrollView;
+    IBOutlet UILabel *ExampleLabel;
+                       
+                       }
+@end
+
