b7cb2a4987254ed0c20ca80d3d9b66cf57ce5bb3
Senglong <>
2018-04-11 14:56:28 -0700

    UI Stuff Be Working - seng (nav)

---
+++ climateConvos/climateConvos/ButtonHandler.h
@@ -0,0 +1,16 @@
+//
+//  ButtonHandler.h
+//  climateConvos
+//
+//  Created by Senglong on 4/10/18.
+//  Copyright © 2018 acn96. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface ButtonHandler : NSObject
+
+@property(nonatomic, strong)UISegmentedControl *segmentControl;
+
+@end
---
+++ climateConvos/climateConvos/EmptySegue.m
@@ -0,0 +1,18 @@
+//
+//  EmptySegue.m
+//  climateConvos
+//
+//  Created by Senglong on 4/9/18.
+//  Copyright © 2018 acn96. All rights reserved.
+//
+
+#import "EmptySegue.h"
+
+@implementation EmptySegue
+
+- (void)perform{
+    
+    
+    
+}
+@end
