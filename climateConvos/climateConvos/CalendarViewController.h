85ed360daee20f5dc03da7d1ea2972e69eb928a2
acn96 <acn96@uw.edu>
2018-04-16 14:36:13 -0700

    DO NOT MERGE YET. IN PROGRESS CALENDAR!

---
+++ climateConvos/climateConvos/CalendarViewController.h
@@ -0,0 +1,13 @@
+//
+//  CalendarViewController.h
+//  climateConvos
+//
+//  Created by acn96 on 4/16/18.
+//  Copyright © 2018 acn96. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalendarViewController : UIViewController
+
+@end
---
+++ climateConvos/climateConvos/CalendarViewController.m
@@ -0,0 +1,48 @@
+//
+//  CalendarViewController.m
+//  climateConvos
+//
+//  Created by acn96 on 4/16/18.
+//  Copyright © 2018 acn96. All rights reserved.
+//
+
+#import "CalendarViewController.h"
+
+@interface CalendarViewController ()
+
+@end
+
+@implementation CalendarViewController
+
+- (void)downloadICS
+{
+    NSString *URLString = @"https://calendar.google.com/calendar/ical/72dh5ehol3oufbkusqagta0qf8%40group.calendar.google.com/public/basic.ics";
+    NSURL *url = [NSURL URLWithString:URLString];
+    NSURLSession *session = [NSURLSession sharedSession];
+    NSURLSessionDataTask *downloadTask = [session dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        // handle response
+        if (error) {
+            NSLog(@"ERROR! - %@", error);
+        } else {
+            [self parseICS:data];
+        }
+    }];
+    
+    [downloadTask resume];
+}
+
+- (void)parseICS:(NSData *)data
+{
+    NSString *ICSAsString = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
+    NSLog(@"%@", ICSAsString);
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    [self downloadICS];
+}
+
+
+
+@end
