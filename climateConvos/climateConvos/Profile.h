0a4b04bd7bd0995d95e1f40da609bc320ef00967
Sandra Le <>
2018-04-21 21:31:03 -0700

    show more page styled (no function tho) + profile segment view works (currently with dummy text)

---
+++ climateConvos/climateConvos/Profile.h
@@ -0,0 +1,19 @@
+//
+//  ListSavedViewController.h
+//  CustomTableViewCells
+//
+//  Created by Sandra Le on 4/20/18.
+//  Copyright © 2018 acn96. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class singleFactoid;
+
+@interface Profile : UIViewController <UITableViewDataSource> {
+    IBOutlet UISegmentedControl *control;
+}
+ - (IBAction)segmentedcontroller;
+// this property is weak because it is "owned" by myTabBarViewController
+//@property (weak, nonatomic) NSArray <singleFactoid *> *drones;
+
+@end
