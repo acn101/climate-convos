bc5839efc4c289febb614cce2ed7b662d332da38
SENG NGOR <ngors@uw.edu>
2018-05-07 12:04:49 -0700

    money monday

--- climateConvos/climateConvos/SettingsViewController.m
+++ climateConvos/climateConvos/SettingsViewController.m
@@ -24,6 +24,7 @@
 @property (weak, nonatomic) IBOutlet UIButton *seattleButton;
 @property (weak, nonatomic) IBOutlet UIButton *houstonButton;
 @property (weak, nonatomic) IBOutlet UILabel *locationLabel;
+@property (weak, nonatomic) IBOutlet UIButton *sourcesButton;
 
 
 @end
@@ -36,7 +37,6 @@
     // Do any additional setup after loading the view.
     [self style];
 
-    
 }
 - (IBAction)aboutUs:(id)sender {
     UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"About Us👋" message:@"\n\n We are SASON, a team of UW students from the Interactive Media Design program and we are creating an iOs app called Climate Convos. Climate Convos creates the agency for well-informed conversations about climate change. It does this through the use of factoids, local articles, and interactive data (live data), which are based on scholarly research and sources. By adding informed dialogue to the world, we believe we can create change to the systems which affect our earth. The information is presented in a way that it advocates face-to-face conversations and can also lead to activism. Users will also be presented with local events and calls-to-action based in the Pacific NorthWest. Due to this localization, we hope to create a sense of urgency as people would then feel a personal stake in the issue.\n" delegate:self cancelButtonTitle:@"Done" otherButtonTitles:nil];
@@ -56,17 +56,22 @@
  
     NSString *valueToSave = @"Seattle";
     [[NSUserDefaults standardUserDefaults] setObject:valueToSave forKey:@"location"];
-     self.seattleButton.backgroundColor=[UIColor colorWithRed:170/255.0 green:170/255.0 blue:170/255.0 alpha:1.0];
-    self.seattleButton.titleLabel.textColor=[UIColor whiteColor];
-    self.seattleButton.layer.cornerRadius = 11;
-    self.seattleButton.layer.masksToBounds = true;
+  self.seattleButton.backgroundColor=[UIColor whiteColor];
+ self.houstonButton.backgroundColor=[UIColor colorWithRed:239/255.0 green:239/255.0 blue:244/255.0 alpha:1.0];
 
+
+    
 }
+
+
 
 - (IBAction)houstonButton:(id)sender {
     NSString *valueToSave = @"Houston";
     [[NSUserDefaults standardUserDefaults] setObject:valueToSave forKey:@"location"];
     [[NSUserDefaults standardUserDefaults] synchronize];
+      self.houstonButton.backgroundColor=[UIColor whiteColor];
+   self.seattleButton.backgroundColor=[UIColor colorWithRed:239/255.0 green:239/255.0 blue:244/255.0 alpha:1.0];
+
 }
 
 
@@ -94,6 +99,13 @@
     self.notificationsLabel.layer.masksToBounds = true;
     self.locationLabel.layer.cornerRadius = 11;
     self.locationLabel.layer.masksToBounds = true;
+    self.seattleButton.layer.cornerRadius = 11;
+    self.seattleButton.layer.masksToBounds = true;
+    self.houstonButton.layer.cornerRadius = 11;
+    self.houstonButton.layer.masksToBounds = true;
+    self.sourcesButton.layer.cornerRadius = 11;
+    self.sourcesButton.layer.masksToBounds = true;
+
 }
 
 
